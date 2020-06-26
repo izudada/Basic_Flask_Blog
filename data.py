f9ce383234281259cb1091b4e931b6196dc2a4fc
Udeagbala Anthony <tonyudeagbala@gmail.com>
2020-06-26 23:51:17 +0000

    Created about page, data file that stores articles which i imported into the app file.

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            'id' : 1,
+            'title' : 'Human Resource Management Software',
+            'body' : 'The perfect combination of different functionality and reporting tools, designed to help your Human Resource Department to deliver the services effectively. We can provide you with such a tool. Our product is designed to meet the needs of any organization in human incentive industry like IT, Oil and Gas Industry, Financial Organization, NGOs, etc. Created for the help of HR professionals, the software helps eliminate many administrative duties, allowing them to focus on the employees and other important tasks which have far reaching impact on your organization',
+            'author' : 'Francis Anaetoh',
+            'create_date' : '26-06-2020'
+        },
+
+        {
+            'id' : 2,
+            'title' : 'School Management Software',
+            'body' : 'Schools have now become large and complex organizations, such that managing the various activities and resources; take a lot of time, effort and resources from teachers, management and authorities. It is a proven fact that teachers spend more than 30% of their time in non-teaching work. With an increase in the number of students in institutions, even routine tasks like recording attendance, providing roll numbers, fee collection, time-table creation, etc. are tedious and time consuming for the staff.',
+            'author' : 'Francis Anaetoh',
+            'create_date' : '27-06-2020'
+        },
+
+        {
+            'id' : 3,
+            'title' : 'Library Management Software',
+            'body' : 'This is a comprehensive solution suitable for all type of libraries. Its flexibility enables it to be installed in a range of Library organizations, ranging from public libraries, through to academic and special libraries. One of the strengths of this software is that it has been developed using the latest technologies.',
+            'author' : 'Nwankwo Samuel',
+            'create_date' : '27-06-2020'
+        }
+    ]
+
+    return articles
