d554677f5392189d18b91d013fd0a09dbf4f3729
Udeagbala Anthony <tonyudeagbala@gmail.com>
2020-06-27 00:09:47 +0000

    Created an article page, and a route for the page

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from data import Articles
 
 app = Flask(__name__)
 
+Articles = Articles()
+
 @app.route("/")
 def index():
         return render_template("home.html")
@@ -11,6 +13,10 @@ def index():
 @app.route("/about")
 def about():
         return render_template("about.html")
+
+@app.route("/articles")
+def articles():
+        return render_template("articles.html", articles = Articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
