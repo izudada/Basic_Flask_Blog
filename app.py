1ea12db73907909277d197c77365eaf61199f751
Udeagbala Anthony <tonyudeagbala@gmail.com>
2020-06-29 16:09:29 +0000

    Another commit

--- app.py
+++ app.py
@@ -18,9 +18,23 @@ mysql = MySQL(app)
 
 Articles = Articles()
 
-@app.route("/")
+class RegisterForm(Form):
+        name = StringField('Name', [validators.length(min=5, max=50)])
+        username = StringField('Username', [validators.length(min=4, max=25)])
+        email = StringField('Email', [validators.length(min=6, max=50)])
+        password = PasswordField('Passowrd', [
+                validators.DataRequired(),
+                validators.EqualTo('confirm', message= 'Passwords do not match')
+        ])
+        confirm = PasswordField('Confirm Password')
+
+@app.route("/", methods = ['GET', 'POST'])
 def index():
-        return render_template("index.html")
+        form = RegisterForm(request.form)
+        if request.method == 'POST' and form.validate():
+                return render_template("index.html")
+
+        return render_template("index.html", form = form)
 
 if __name__ == '__main__':
     app.run(debug=True)
